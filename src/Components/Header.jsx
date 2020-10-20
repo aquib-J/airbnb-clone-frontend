c2bd0867148705ece8720f0aa5fab4f4f836cc47
Raghav Singhal <raghav.singhal@mountblue.tech>
2020-10-20 14:08:49 +0530

    Add Search Bar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+// import DatePicker from 'react-datepicker';
+// import 'react-datepicker/dist/react-datepicker';
 
 import {
   Box,
@@ -15,6 +17,10 @@ import {
   Icon,
   Text,
   PseudoBox,
+  ButtonGroup,
+  Divider,
+  Input,
+  Stack,
 } from "@chakra-ui/core";
 import Logo from "../Assets/logo3.png";
 import { AiOutlineGlobal, AiOutlineUser } from "react-icons/ai";
@@ -38,7 +44,7 @@ class Header extends Component {
   render() {
     return (
       <Box px="80px" w="100%" position="fixed" bg="white">
-        <Flex justify="space-between" py={4}>
+        <Flex justify="space-between" py={4} flexWrap="wrap">
           <Flex alignItems="flex-start" pt={2}>
             <Image h="32px" w="102px" src={Logo} alt="Logo" cursor="pointer" />
           </Flex>
@@ -52,6 +58,7 @@ class Header extends Component {
               borderColor="#dddddd"
               borderRadius={50}
               onClick={this.handleToggle}
+              flex={1}
               outline="none"
               justifyContent="space-between"
               alignItems="center"
@@ -66,7 +73,6 @@ class Header extends Component {
                 <Icon name="search-2" color="white" size="12px" />
               </Box>
             </PseudoBox>
-            <Collapse isOpen={this.state.show} w={200}></Collapse>
           </Flex>
 
           <Box>
@@ -112,6 +118,99 @@ class Header extends Component {
               </MenuList>
             </Menu>
           </Box>
+          <Box w="100%"></Box>
+          <Flex w="100%" justify="center" mt={4}>
+            <Collapse isOpen={this.state.show}>
+              <Flex
+                align="center"
+                border="1px"
+                borderRadius="50px"
+                borderColor="#dddddd"
+                fontSize="14px"
+              >
+                <Flex
+                  variantColor="teal"
+                  variant="ghost"
+                  rounded="50px"
+                  pl={5}
+                  my={0}
+                  flexDirection="column"
+                  justify="center"
+                  display="inline-flex"
+                  _hover={{ bg: "#ebebeb" }}
+                >
+                  <Text fontWeight={600}>Location</Text>
+                  <Input
+                    variant="unstyled"
+                    placeholder="Where are you going?"
+                    size="sm"
+                  />
+                </Flex>
+                <Button
+                  rounded="50px"
+                  bg="none"
+                  px={16}
+                  py={8}
+                  m="0"
+                  as="button"
+                  _hover={{ bg: "#ebebeb" }}
+                >
+                  <Stack spacing={0} textAlign="left">
+                    <Box>
+                      <Text fontWeight={600}>Check In</Text>
+                    </Box>
+                    <Box>
+                      <Text color="#adb1c6" fontWeight="200">
+                        Add Dates
+                      </Text>
+                    </Box>
+                  </Stack>
+                </Button>
+                <Button
+                  rounded="50px"
+                  px={16}
+                  py={8}
+                  bg="none"
+                  m="0"
+                  as="button"
+                  _hover={{ bg: "#ebebeb" }}
+                >
+                  <Stack spacing={0} textAlign="left">
+                    <Box>
+                      <Text fontWeight={600}>Check In</Text>
+                    </Box>
+                    <Box>
+                      <Text color="#adb1c6" fontWeight="200">
+                        Add Dates
+                      </Text>
+                    </Box>
+                  </Stack>
+                </Button>
+                <Button
+                  rounded="50px"
+                  px={8}
+                  py={8}
+                  m="0"
+                  bg="none"
+                  _hover={{ bg: "#ebebeb" }}
+                >
+                  <Stack spacing={0} mr={10} textAlign="left">
+                    <Box>
+                      <Text fontWeight={600}>Check In</Text>
+                    </Box>
+                    <Box>
+                      <Text color="#adb1c6" fontWeight="200">
+                        Add Dates
+                      </Text>
+                    </Box>
+                  </Stack>
+                  <Box borderRadius={50} p={3} bg="#f3575e">
+                    <Icon name="search-2" color="white" size="22px" />
+                  </Box>
+                </Button>
+              </Flex>
+            </Collapse>
+          </Flex>
         </Flex>
       </Box>
     );
