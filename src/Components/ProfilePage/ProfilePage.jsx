b1a7a7673817ec853750eed0dfd6c1d354f11c3f
Raghav Singhal <raghav.singhal@mountblue.tech>
2020-10-28 13:41:47 +0530

    Add the booking history and your lisitng accordion

--- src/Components/ProfilePage/ProfilePage.jsx
+++ src/Components/ProfilePage/ProfilePage.jsx
@@ -1,6 +1,19 @@
-import { Avatar, Box, Flex, Text } from "@chakra-ui/core";
+import {
+  Avatar,
+  Box,
+  Flex,
+  Text,
+  Accordion,
+  AccordionItem,
+  AccordionHeader,
+  AccordionPanel,
+  Button,
+  Heading,
+} from "@chakra-ui/core";
 import React, { Component } from "react";
 import EditableBio from "./EditableBio";
+import BookingHistory from "./BookingHistory";
+import YourListings from "./YourListings";
 
 class ProfilePage extends Component {
   render() {
@@ -9,7 +22,6 @@ class ProfilePage extends Component {
         <Text>Profile</Text>
         <Flex w="100%">
           <EditableBio />
-          {/* <Flex justify="center" w="25%"> */}
           <Avatar
             h={200}
             w={200}
@@ -17,8 +29,64 @@ class ProfilePage extends Component {
             src="https://bit.ly/sage-adebayo"
             mx="auto"
           />
-          {/* </Flex> */}
         </Flex>
+        <Accordion allowToggle py={5}>
+          <AccordionItem borderTop="none" defaultIsOpen={false}>
+            {({ isExpanded }) => (
+              <>
+                <Flex justify="space-between">
+                  <Box flex="100%" pb={isExpanded ? 4 : 8} pt={8}>
+                    <Heading>Booking history</Heading>
+                  </Box>
+                  <AccordionHeader
+                    flex={1}
+                    _hover={{ bg: "none" }}
+                    _focus={{ outline: "none" }}
+                  >
+                    <Button
+                      variant="link"
+                      variantColor="teal"
+                      _focus={{ outline: "none" }}
+                    >
+                      {isExpanded ? "Collapse" : "Expand"}
+                    </Button>
+                  </AccordionHeader>
+                </Flex>
+                <AccordionPanel mb={4}>
+                  <BookingHistory />
+                </AccordionPanel>
+              </>
+            )}
+          </AccordionItem>
+
+          <AccordionItem>
+            {({ isExpanded }) => (
+              <>
+                <Flex justify="space-between">
+                  <Box flex="100%" pb={isExpanded ? 4 : 8} pt={8}>
+                    <Heading>Your Listings</Heading>
+                  </Box>
+                  <AccordionHeader
+                    flex={1}
+                    _hover={{ bg: "none" }}
+                    _focus={{ outline: "none" }}
+                  >
+                    <Button
+                      variant="link"
+                      variantColor="teal"
+                      _focus={{ outline: "none" }}
+                    >
+                      {isExpanded ? "Collapse" : "Expand"}
+                    </Button>
+                  </AccordionHeader>
+                </Flex>
+                <AccordionPanel mb={4}>
+                  <YourListings />
+                </AccordionPanel>
+              </>
+            )}
+          </AccordionItem>
+        </Accordion>
       </Box>
     );
   }
