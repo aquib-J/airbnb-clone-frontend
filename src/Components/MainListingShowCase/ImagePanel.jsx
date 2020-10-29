b53f11d8b6132f93539288f4eb6eec7f23227f0e
Raghav Singhal <raghav.singhal@mountblue.tech>
2020-10-29 17:16:41 +0530

    Alter the image issue on listings page

--- src/Components/MainListingShowCase/ImagePanel.jsx
+++ src/Components/MainListingShowCase/ImagePanel.jsx
@@ -7,7 +7,11 @@ class ImagePanel extends Component {
       <SimpleGrid w="100%" minChildWidth="50%" rounded="lg" overflow="hidden">
         <Box mr={2}>
           <Image
-            src={this.props.url[0].url}
+            src={
+              this.props.url[0]
+                ? this.props.url[0].url
+                : "https://www.gingerhotels.com/resourcefiles/hotelprofile/ginger-thane-hotel-th.jpg"
+            }
             alt="Dan Abramov"
             minWidth="100%"
             minHeight="100%"
@@ -16,7 +20,11 @@ class ImagePanel extends Component {
         <SimpleGrid minChildWidth="50%">
           <Box mr={2} mb={2}>
             <Image
-              src={this.props.url[1].url}
+              src={
+                this.props.url[1]
+                  ? this.props.url[1].url
+                  : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJqGhZNdfVkVcBUXzMf0RYLRVbCPzY5x9OYw&usqp=CAU"
+              }
               alt="Dan Abramov"
               minWidth="100%"
               minHeight="100%"
@@ -24,7 +32,11 @@ class ImagePanel extends Component {
           </Box>
           <Box mb={2}>
             <Image
-              src={this.props.url[2].url}
+              src={
+                this.props.url[2]
+                  ? this.props.url[2].url
+                  : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTid-W_Us-CPVs8G54plpO2py_yn55FLEK-lg&usqp=CAU"
+              }
               alt="Dan Abramov"
               minWidth="100%"
               minHeight="100%"
@@ -32,7 +44,11 @@ class ImagePanel extends Component {
           </Box>
           <Box mr={2}>
             <Image
-              src={this.props.url[3].url}
+              src={
+                this.props.url[3]
+                  ? this.props.url[3].url
+                  : "https://www.gingerhotels.com/resourcefiles/hotelprofile/ginger-thane-hotel-th.jpg"
+              }
               alt="Dan Abramov"
               minWidth="100%"
               minHeight="100%"
@@ -40,7 +56,11 @@ class ImagePanel extends Component {
           </Box>
           <Box>
             <Image
-              src={this.props.url[4].url}
+              src={
+                this.props.url[4]
+                  ? this.props.url[4].url
+                  : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREKh1dg82cxs6c_zev7y_MkgdljVuH_dQo9A&usqp=CAU"
+              }
               alt="Dan Abramov"
               minWidth="100%"
               minHeight="100%"
