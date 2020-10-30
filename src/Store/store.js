0013b1c51d683023cf50d46b3a2c6082451cf280
Raghav Singhal <raghav.singhal@mountblue.tech>
2020-10-30 18:04:24 +0530

    Alter the login bug when refreshed

--- src/Store/store.js
+++ src/Store/store.js
@@ -2,9 +2,16 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import api from "./middleware/getLisitngApi";
 import reducer from "./reducer";
 
+let initialState = {
+  user: JSON.parse(localStorage.getItem("user"))
+    ? { currentUser: JSON.parse(localStorage.getItem("user")) }
+    : {},
+};
+
 const store = configureStore({
   reducer,
   middleware: [...getDefaultMiddleware(), api],
+  preloadedState: initialState,
 });
 
 export default store;
